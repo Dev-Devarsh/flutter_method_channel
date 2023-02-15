79a7ff4bd153d73c5da7f24ed134ac237caf82ab
Devarsh-AD <devarsh@simpos.com.au>
2023-02-15 10:12:38 +0530

    implement Event channel in flutter

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
@@ -66,6 +67,7 @@ class _HomePageState extends State<HomePage> {
       if (call.method == 'reportBatteryLevel') {
         listenBattery = call.arguments as int;
       }
+      setState(() {});
     });
     log('batterLevel');
     setState(() {});
